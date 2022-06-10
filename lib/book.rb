896a4a336482146eb160c6016740a8c42f97406e
Kenyansa <kenyansajustine@gmail.com>
2022-06-10 10:03:01 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+class Book
+    attr_reader :title
+    attr_accessor :page_count, :author, :genre
+    def initialize(title)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+book = Book.new("And Then There Were None")
+author = Book.new("Agatha Christie")
+page_count = Book.new("272")
+genre = ("Mystery")
+book = Book.new("The World According to Garp")
+
+
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :color, :material, :condition, :size
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+        @color = color
+        @material = material
+        @condition = condition
+        @size = size
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
+shoe = Shoe.new("Adidas")
+brand = Shoe.new("Nike")
+color = Shoe.new("red")
+size = Shoe.new(9.5)
+material = Shoe.new("suede")
+condition = Shoe.new("tattered")
